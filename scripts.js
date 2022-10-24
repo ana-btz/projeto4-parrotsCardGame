ed4a379faa13d332b9e2acf45823bdc8487ee4fe
ana-btz <silva.anabtz@gmail.com>
2022-10-24 09:47:55 -0300

    feat: adicionada a funcionalidade que insere as cartas na página de forma aleatória, implementação de layout responsivo

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
 // variaveis globais
+
 let qtd = parseInt(prompt("Com quantas cartas você deseja jogar?"));
 
+const papagaio1 = "<img src='./media/bobrossparrot.gif'";
+const papagaio2 = "<img src='./media/fiestaparrot.gif'";
+const papagaio3 = "<img src='./media/metalparrot.gif'";
+const papagaio4 = "<img src='./media/revertitparrot.gif'";
+const papagaio5 = "<img src='./media/tripletsparrot.gif'";
+const papagaio6 = "<img src='./media/unicornparrot.gif'";
+const papagaio7 = "<img src='./media/mineparrot.gif'";
+
+const gifs = [
+    papagaio1, papagaio1, 
+    papagaio2, papagaio2, 
+    papagaio3, papagaio3, 
+    papagaio4, papagaio4, 
+    papagaio5, papagaio5, 
+    papagaio6, papagaio6, 
+    papagaio7, papagaio7];
+
+let cartas = [];
 
 // feature que pergunta ao usuario com quantas cartas ele deseja jogar
+
 while ((qtd < 4) || (qtd > 14) || (isNaN(qtd)) || (qtd % 2 !== 0)) {
 
     if (isNaN(qtd)) {
@@ -15,19 +35,60 @@ while ((qtd < 4) || (qtd > 14) || (isNaN(qtd)) || (qtd % 2 !== 0)) {
 
 }
 
-console.log(qtd);
-typeof (qtd);
+// funçao que gera e armazena as cartas de acordo com a quantidade de cartas informada pelo usuário
 
+function geraCartas() {
+    // pegar cada gif do array e adicionar na tela como <li> dentro da <ul>
+    const deck = document.querySelector("ul");
 
-// feature que gera e armazena as cartas de acordo com a quantidade de cartas informada pelo usuário
-// ...
+    for (let i = 0; i < qtd; i++) {
+        const carta = `
+            <li class="carta" class="${i}" onclick="viraCarta()">
+                <div class="face frente" >
+                    <img src='./media/front 1.png'>
+                </div>
+                <div class="face verso">
+                    ${gifs[i]}
+                </div>
+            </li>`
 
+        cartas.push(carta);
+    }
+    
+    cartas.sort(comparador);
+    deck.innerHTML += cartas;    
+}
+geraCartas();
 
-// funçao que vira a carta ao ser clicada
-function virarCarta() {
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+// funçao que vira a cartinha ao ser clicada
+
+function viraCarta() {
     const frente = document.querySelector(".frente");
     const verso = document.querySelector(".verso");
 
-    frente.classList.add("frente-onclick");
-    verso.classList.add("verso-onclick");
+    for (let i = 0; i < qtd; i++) {
+        frente.classList.add("frente-onclick");
+        verso.classList.add("verso-onclick");
+    }
+
+    // setTimeout(desviraCarta, 1000);
 }
+
+// function viraCartaVerso() {    
+
+//     for (let i = 0; i < qtd; i++) {
+//         const verso = document.querySelector(`.$[i] .verso"`);
+//         verso.classList.add("verso-onclick");
+//     }
+
+// }
+
+// function desviraCarta() {
+//     frente.classList.remove("frente-onclick");
+//     verso.classList.remove("verso-onclick");
+// }
+
